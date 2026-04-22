f4b1f45cda6216537075c60585cb22867df2dcfa
PaulPayumo-design <jpspayumo.student@ua.edu.ph>
2026-04-22 23:59:39 +0800

    chore: add dummy postcss.config.js for deployment compatibility

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,2 @@
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-};
+// This file intentionally left blank to prevent build errors on platforms that expect postcss.config.js.
+// The actual config is in postcss.config.cjs for ESM compatibility.
